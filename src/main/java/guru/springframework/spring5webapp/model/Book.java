f6421322abe5fca5065d22078c1f4df912d1fc82
Johanes Anggara <johanes.p.a.s@gmail.com>
2018-05-01 21:58:36 +0700

    Second Commit

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -12,7 +12,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -22,13 +24,14 @@ public class Book {
     public Book(){
 
     }
-    public Book(String title, String isbn, String publisher){
+
+    public Book(String title, String isbn, Publisher publisher){
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors){
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors){
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -62,9 +65,9 @@ public class Book {
 
     public void setIsbn(String isbn) { this.isbn = isbn;}
 
-    public String getPublisher() { return publisher;}
+    public Publisher getPublisher() { return publisher;}
 
-    public void setPublisher(String publisher) { this.publisher = publisher;}
+    public void setPublisher(Publisher publisher) { this.publisher = publisher;}
 
     public Set<Author> getAuthors() { return authors;}
 
